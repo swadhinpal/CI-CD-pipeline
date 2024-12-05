e70537ab1167849d90af2dad5d4b78e176f26b6b
swadhinpal <bsse1302@iit.du.ac.bd>
2024-12-05 22:01:10 +0600

    CI is created for testing

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+console.log('Initializing server...');
+
+app.get('/', (req, res) => {
+    console.log('Received GET request at /');
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
+
+module.exports = app;
---
+++ test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./app');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('GET /', () => {
+    it('should return Hello World', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res.text).to.equal('Hello World');
+                done();
+            });
+    });
+});
