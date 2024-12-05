83d7ba0cc472d480db1407e0e35c4d627b704013
swadhinpal <bsse1302@iit.du.ac.bd>
2024-12-06 01:20:38 +0600

    CI is created for testing9

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000; // Default to 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+  
 
 const server = app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`);
 });
 
+app.get('/', (req, res) => {
+    try {
+      res.send('Hello World');
+    } catch (err) {
+      console.error("Error handling request:", err);
+      res.status(500).send('Something went wrong');
+    }
+  });
+  
 // Export the server for tests
 export { server, app };
